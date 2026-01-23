9fd37382b78cc5b047b325c8209caf8b9021098c
Vaibhav Sharma <vaibhav.sharma.dev.2001@gmail.com>
2026-01-23 08:05:40 +0530

    Ignore Android .cxx build artifacts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,47 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+// import { NewAppScreen } from '@react-native/new-app-screen';
+// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import {
+//   SafeAreaProvider,
+//   useSafeAreaInsets,
+// } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
 
+//   return (
+//     <SafeAreaProvider>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <AppContent />
+//     </SafeAreaProvider>
+//   );
+// }
+
+// function AppContent() {
+//   const safeAreaInsets = useSafeAreaInsets();
+
+//   return (
+//     <View style={styles.container}>
+//       <NewAppScreen
+//         templateFileName="App.tsx"
+//         safeAreaInsets={safeAreaInsets}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// export default App;
+
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import {Provider} from "react-redux";
+import {store} from "./src/store/store.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/Home.jsx";
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Property" component={Home} />
+            <Stack.Screen name="Confirm-Booking" component={Home} />
+            <Stack.Screen name="Saved" component={Home} />
+            <Stack.Screen name="Trips" component={Home} />
+            <Stack.Screen name="Profile" component={Home} />
+            <Stack.Screen name="Signup" component={Home} />
+            <Stack.Screen name="Login" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
+  )
 }
-
-// vola
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
---
+++ mobile/src/screens/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context"
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+            <View>
+
+            <Text>Home Screen</Text>
+            <Text>Home Screen</Text>
+            <Text>Home Screen</Text>
+            <Text>Home Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
